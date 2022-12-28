6a1df76c8bb957599508f5cb96718e30c9e68260
BillyMaysHere32 <kgetz32@gmail.com>
2022-12-27 16:25:45 -0800

    Connected application to mongoDb and created routes/controllers folders with files for posts

--- server/index.js
+++ server/index.js
@@ -2,8 +2,25 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-// 30mb limit for images
+app.use('/posts', postRoutes);
+
+// set up body parser to send requests, 30mb limit for images
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+
+app.use(cors());
+
+//set the strictQuery option to true globally to suppress the warning
+mongoose.set('strictQuery', true);
+
+// DB Connection
+const mongoDb = "mongodb+srv://admin:1234@cluster0.ag5ph2d.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT|| 4000;
+mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true })
+    .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+    .catch((error) => console.log(`${error} did not connect`));
+
