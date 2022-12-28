f976d29bd7b95a5477b9b0873ef283297113e1cf
BillyMaysHere32 <kgetz32@gmail.com>
2022-12-28 11:36:05 -0800

    added createPost and started working on frontend with app.js title and image

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from '@mui/material';
+import logo from './images/logo.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <h1>
-        App
-      </h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography  variant="h2" align="center">Social Media App</Typography>
+        <img src={logo} alt="icon" height="60" />
+      </AppBar>
+    </Container>
   );
-}
+};
 
 export default App;
