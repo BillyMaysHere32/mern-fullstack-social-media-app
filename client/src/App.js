ee385e65bd0ca69516122129052c332755792528
BillyMaysHere32 <kgetz32@gmail.com>
2022-12-28 13:36:31 -0800

    created styles for posts

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,31 @@
 import React from "react";
 import { Container, AppBar, Typography, Grow, Grid } from '@mui/material';
 import logo from './images/logo.jpg';
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+import useStyles from './styles';
 
 function App() {
+  const classes = useStyles();
+
   return (
     <Container maxWidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography  variant="h2" align="center">Social Media App</Typography>
-        <img src={logo} alt="icon" height="60" />
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">Social Media App</Typography>
+        <img className={classes.image} src={logo} alt="icon" height="150" />
       </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
     </Container>
   );
 };
