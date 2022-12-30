9060910fc0f361e0c857fe7e446fb0f8edf34b42
BillyMaysHere32 <kgetz32@gmail.com>
2022-12-30 13:05:00 -0800

    created posts logic to return all posts

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,19 +1,25 @@
 import React from 'react'
+import { Grid, CircularProgress } from '@mui/material'
 import { useSelector } from 'react-redux';
 import Post from './Post/Post'
-//import useStyles from './styles';
+import useStyles from './styles';
 
 export default function Posts() {
+  //fetch data from global redux store
   const posts = useSelector((state) => state.posts);
-  //const classes = useStyles();
+  const classes = useStyles();
   
   console.log(posts);
 
   return (
-    <div>
-      <h1>Posts</h1>
-      <Post />
-      <Post />
-    </div>
+    !posts.length ? <CircularProgress /> : (
+      <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+         {posts.map((post) => (
+          <Grid key={post._id} item xs={12} sm={6} md={6}>
+            <Post  />
+          </Grid>
+        ))}
+      </Grid>
+    )
   )
 }
